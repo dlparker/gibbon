9a59567d53e9982b9a0d0c2c024bc4df4a8c885e
Dennis Parker <dennis@windhavengroup.com>
2026-01-08 14:55:36 -0600

    removed a bunch of failed experiements

--- scripts/aim_tree_sink.py
+++
@@ -1,9 +0,0 @@
-import yaml
-import re
-from pathlib import Path
-from typing import List, Optional
-from gibbon.drafts.draft_sink import DraftSink, DraftContext, SinkResponse
-
-class AimTreeEditor(DraftSink):
-
-    pass
--- scripts/do_sinks.py
+++
@@ -1,7 +0,0 @@
-from trial_sinks import SinkRegistry, TreeNavSink, NotesSink
-
-regy = SinkRegistry()
-regy.register(TreeNavSink())
-regy.register(NotesSink())
-lines = regy.get_topics_for_prompt()
-print(lines)
